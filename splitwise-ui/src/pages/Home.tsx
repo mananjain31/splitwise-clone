73edd2341630a4fd3404ddc947fcc3cf8e572752
mananjain31 <mananjain31jan@gmail.com>
2024-11-09 02:57:20 +0530

    :lipstick: minor homepage change

--- splitwise-ui/src/pages/Home.tsx
+++ splitwise-ui/src/pages/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <span className="text-red-400"> Mathing?</span>
         </Heading>
         <img
-          className="w-3/4 sm:w-1/2"
+          className="w-3/4 sm:w-1/2 border-8 border-yellow-200"
           src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2s1bDNlZWM0cHd4OGtqYXZueG5zb2F6OGJiNjltZnJyZDZrdWNobSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lHfxDepSGlzom6f65K/giphy.gif"
         />
         <Heading className="text-green-400 mt-2 text-5xl">
